7c5790a0be99b9b5af83578aec408a21e660d1e2
root <slater.stich@gmail.com>
2016-03-12 01:20:12 -0500

    first working version

--- extract.py
+++ extract.py
@@ -3,19 +3,23 @@ import pandas as pd
 import datetime
 import uuid
 import numpy as np
+import time
 
 def _solution_length(solution):
-    if solution is None:
-        return 0
-    if type(solution) == float:
-        return -1
-    else:
-        return
+    try:
+        return len(solution)
+    except:
+        if solution is None:
+            return 0
+        elif isinstance(solution, (int, float, np.int64)):
+            return -1
+        else:
+            raise Exception('Unknown input type: {}'.format(solution))
 
 def time_limit_filter(df, millisecond_limit):
-    start = df['completedDate'].map(int).min()
+    start = int(df['completedDate'].min())
     middle = start + millisecond_limit
-    end = df['completedDate'].map(int).max()
+    end = int(df['completedDate'].max())
 
     before_cutoff = df[(df['completedDate'] >= start) & (df['completedDate'] <= middle)]
     after_cutoff = df[(df['completedDate'] > middle) & (df['completedDate'] <= end)]
@@ -27,8 +31,8 @@ def feature_extractor(df):
     feature_dict = {}
 
     if 'solution' not in df.columns:
-        df['solution'] = -1
-    df['solution'].fillna(-1, inplace=True)
+        df['solution'] = ''
+    df['solution'].fillna('', inplace=True)
     if 'name' not in df.columns:
         df['name'] = ''
     df['name'].fillna('', inplace=True)
@@ -49,7 +53,7 @@ def feature_extractor(df):
     feature_dict['num_empty_solutions'] = (solution_lengths == 0).sum()
     feature_dict['num_unsolved'] = (solution_lengths == -1).sum()
 
-    time_splits = np.diff(df['completedDate'])
+    time_splits = np.diff(sorted(df['completedDate'].tolist()))
     if len(time_splits) == 0:
         time_splits = np.array([-1])
     feature_dict['min_sec_between_solutions'] = time_splits.min()
@@ -71,15 +75,20 @@ def readline(line_string):
 if __name__ == '__main__':
     user_data = []
     with open('output.json') as stream:
+        start_time = time.time()
         for line in stream:
             line = line.strip()
             if line == '[' or line == ']':
                 continue
             df = readline(line)
-            before, after = time_limit_filter(df, 1000*60*60*24*7)
+            if not set(df.columns).issubset(set(['name', 'completedDate', 'solution'])):
+                raise Exception(df.columns)
+            before, after = time_limit_filter(df, 1000*60*60*24*14)
 	    before = feature_extractor(before)
             before['after_days'] = len(after)
             user_data.append(before)
-            print len(user_data)/100000.0
+            if len(user_data) % 1000 == 0:
+                print len(user_data)/100000.0, time.time() - start_time
 
     user_data = pd.DataFrame(user_data)
+    user_data.to_csv('user_data.csv')
